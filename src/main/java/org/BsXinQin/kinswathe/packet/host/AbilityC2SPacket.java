ed15bfe17193cc24d7fb99496d766efe25ec514a
Relicn-zero <3373451267@qq.com>
2026-05-16 02:41:33 +0800

    Update AbilityC2SPacket.java

--- src/main/java/org/BsXinQin/kinswathe/packet/host/AbilityC2SPacket.java
+++ src/main/java/org/BsXinQin/kinswathe/packet/host/AbilityC2SPacket.java
@@ -1,22 +1,21 @@
 package org.BsXinQin.kinswathe.packet.host;
 
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 import org.BsXinQin.kinswathe.KinsWathe;
-import org.jetbrains.annotations.NotNull;
 
 public record AbilityC2SPacket() implements CustomPayload {
-
     public static final Identifier ABILITY_PAYLOAD_ID = Identifier.of(KinsWathe.MOD_ID, "ability");
     public static final Id<AbilityC2SPacket> ID = new Id<>(ABILITY_PAYLOAD_ID);
-    public static final PacketCodec<RegistryByteBuf, AbilityC2SPacket> CODEC;
-    static {CODEC = PacketCodec.of(AbilityC2SPacket::write, AbilityC2SPacket::read);}
-    public void write(PacketByteBuf buf) {}
-    public static AbilityC2SPacket read(PacketByteBuf buf) {
-        return new AbilityC2SPacket();
+    public static final PacketCodec<PacketByteBuf, AbilityC2SPacket> CODEC = PacketCodec.of(
+        (value, buf) -> {},
+        buf -> new AbilityC2SPacket()
+    );
+
+    @Override
+    public Id<? extends CustomPayload> getId() {
+        return ID;
     }
-    public @NotNull Id<? extends @NotNull CustomPayload> getId() {return ID;}
 }
