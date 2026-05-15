05247e36779be543c26c064460230c1594a04efd
Relicn-zero <3373451267@qq.com>
2026-05-16 02:22:36 +0800

    Update ArbiterAbility.java

--- src/main/java/org/BsXinQin/kinswathe/roles/arbiter/ArbiterAbility.java
+++ src/main/java/org/BsXinQin/kinswathe/roles/arbiter/ArbiterAbility.java
@@ -6,30 +6,31 @@ import dev.doctor4t.wathe.game.GameFunctions;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import org.BsXinQin.kinswathe.KinsWatheConfig;
 import org.BsXinQin.kinswathe.KinsWatheRoles;
 import org.BsXinQin.kinswathe.component.AbilityPlayerComponent;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.UUID;
-
 public class ArbiterAbility {
 
-    public static void register(@NotNull PlayerEntity player, UUID targetUuid) {
-        ServerPlayerEntity target = player.getServer().getPlayerManager().getPlayer(targetUuid);
-        if (target == null) return;
-
+    public static void register(@NotNull PlayerEntity player) {
         GameWorldComponent gameWorld = GameWorldComponent.KEY.get(player.getWorld());
         AbilityPlayerComponent ability = AbilityPlayerComponent.KEY.get(player);
         PlayerShopComponent playerShop = PlayerShopComponent.KEY.get(player);
 
         if (gameWorld.isRole(player, KinsWatheRoles.ARBITER) && GameFunctions.isPlayerAliveAndSurvival(player) && ability.cooldown <= 0) {
             if (playerShop.balance < KinsWatheConfig.HANDLER.instance().ArbiterAbilityPrice) return;
-            if (player.getUuid().equals(target.getUuid())) return;
+
+            HitResult hitResult = ProjectileUtil.getCollision(player, entity -> entity instanceof ServerPlayerEntity target && GameFunctions.isPlayerAliveAndSurvival(target), 2.0f);
+            ServerPlayerEntity target = (hitResult instanceof EntityHitResult entityHitResult) ? (ServerPlayerEntity) entityHitResult.getEntity() : null;
+            if (target == null || player.getUuid().equals(target.getUuid())) return;
 
             playerShop.balance -= KinsWatheConfig.HANDLER.instance().ArbiterAbilityPrice;
             playerShop.sync();
