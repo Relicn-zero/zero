c35bd11c588e7e09f8cd948e2a93c67512fcd12a
Relicn-zero <3373451267@qq.com>
2026-05-16 03:20:02 +0800

    Update and rename ArbiterPlayerWidget.java to ArbiterHudMixin.java

---
+++ src/client/java/org/BsXinQin/kinswathe/client/mixin/roles/arbiter/ArbiterHudMixin.java
@@ -0,0 +1,53 @@
+package org.BsXinQin.kinswathe.client.mixin.roles.arbiter;
+
+import dev.doctor4t.wathe.cca.GameWorldComponent;
+import dev.doctor4t.wathe.cca.PlayerShopComponent;
+import dev.doctor4t.wathe.client.WatheClient;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.text.Text;
+import org.BsXinQin.kinswathe.KinsWatheRoles;
+import org.BsXinQin.kinswathe.client.KinsWatheInitializeClient;
+import org.BsXinQin.kinswathe.component.AbilityPlayerComponent;
+import org.BsXinQin.kinswathe.component.ConfigWorldComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public abstract class ArbiterHudMixin {
+
+    @Shadow public abstract TextRenderer getTextRenderer();
+
+    @Inject(method = "render", at = @At("TAIL"))
+    public void getAbilityHud(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        var player = MinecraftClient.getInstance().player;
+        if (player == null) return;
+
+        var gameWorld = GameWorldComponent.KEY.get(player.getWorld());
+        var ability = AbilityPlayerComponent.KEY.get(player);
+        var playerShop = PlayerShopComponent.KEY.get(player);
+        var config = ConfigWorldComponent.KEY.get(player.getWorld());
+
+        if (gameWorld.isRole(player, KinsWatheRoles.ARBITER) && WatheClient.isPlayerAliveAndInSurvival()) {
+            int drawY = context.getScaledWindowHeight();
+            Text line;
+            if (playerShop.balance < config.ArbiterAbilityPrice) {
+                line = Text.translatable("tip.kinswathe.ability.not_enough_money", config.ArbiterAbilityPrice);
+            } else if (ability.cooldown > 0) {
+                line = Text.translatable("tip.kinswathe.cooldown", ability.cooldown / 20);
+            } else {
+                line = Text.translatable("tip.kinswathe.ability.can_use", KinsWatheInitializeClient.abilityBind.getBoundKeyLocalizedText());
+            }
+            drawY -= getTextRenderer().getWrappedLinesHeight(line, 999999);
+            context.drawTextWithShadow(getTextRenderer(), line,
+                context.getScaledWindowWidth() - getTextRenderer().getWidth(line),
+                drawY, KinsWatheRoles.ARBITER.color());
+        }
+    }
+}
