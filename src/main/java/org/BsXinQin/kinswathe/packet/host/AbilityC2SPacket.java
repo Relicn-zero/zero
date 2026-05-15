1db2e6e7a21ab25d8fa7aa6a81abb9a7ec65369d
Relicn-zero <3373451267@qq.com>
2026-05-16 01:04:17 +0800

    Update AbilityC2SPacket.java

--- src/main/java/org/BsXinQin/kinswathe/packet/host/AbilityC2SPacket.java
+++ src/main/java/org/BsXinQin/kinswathe/packet/host/AbilityC2SPacket.java
@@ -1,22 +1,35 @@
-package org.BsXinQin.kinswathe.packet.host;
+package org.BsXinQin.kinswathe.packet.roles;
 
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 import org.BsXinQin.kinswathe.KinsWathe;
+import org.BsXinQin.kinswathe.roles.arbiter.ArbiterAbility;
 import org.jetbrains.annotations.NotNull;
 
-public record AbilityC2SPacket() implements CustomPayload {
+import java.util.UUID;
 
-    public static final Identifier ABILITY_PAYLOAD_ID = Identifier.of(KinsWathe.MOD_ID, "ability");
-    public static final Id<AbilityC2SPacket> ID = new Id<>(ABILITY_PAYLOAD_ID);
-    public static final PacketCodec<RegistryByteBuf, AbilityC2SPacket> CODEC;
-    static {CODEC = PacketCodec.of(AbilityC2SPacket::write, AbilityC2SPacket::read);}
-    public void write(PacketByteBuf buf) {}
-    public static AbilityC2SPacket read(PacketByteBuf buf) {
-        return new AbilityC2SPacket();
+public record ArbiterC2SPacket(UUID targetUuid) implements CustomPayload {
+    public static final Identifier ID = Identifier.of(KinsWathe.MOD_ID, "arbiter");
+    public static final Id<ArbiterC2SPacket> PACKET_ID = new Id<>(ID);
+    public static final PacketCodec<PacketByteBuf, ArbiterC2SPacket> CODEC = PacketCodec.tuple(
+        PacketCodecs.UUID, ArbiterC2SPacket::targetUuid,
+        ArbiterC2SPacket::new
+    );
+
+    @Override
+    @NotNull
+    public Id<? extends CustomPayload> getId() {
+        return PACKET_ID;
     }
-    public @NotNull Id<? extends @NotNull CustomPayload> getId() {return ID;}
+
+    public static class Receiver implements ServerPlayNetworking.PlayPayloadHandler<ArbiterC2SPacket> {
+        @Override
+        public void receive(@NotNull ArbiterC2SPacket payload, @NotNull ServerPlayNetworking.Context context) {
+            ArbiterAbility.register(context.player(), payload.targetUuid());
+        }
+    }
 }
