65e0fd96df074e7450baa12c2bf21f4ace8719a2
Relicn-zero <3373451267@qq.com>
2026-05-16 10:42:14 +0800

    Update IncomeIconMixin.java

--- src/client/java/org/BsXinQin/kinswathe/client/mixin/gui/IncomeIconMixin.java
+++ src/client/java/org/BsXinQin/kinswathe/client/mixin/gui/IncomeIconMixin.java
@@ -37,7 +37,8 @@ public abstract class IncomeIconMixin {
                 GameWorldComponent.KEY.get(player.getWorld()).isRole(player, KinsWatheRoles.LICENSED_VILLAIN) ||
                 GameWorldComponent.KEY.get(player.getWorld()).isRole(player, KinsWatheRoles.PHYSICIAN) ||
                 GameWorldComponent.KEY.get(player.getWorld()).isRole(player, KinsWatheRoles.TECHNICIAN) ||
-               (ConfigWorldComponent.KEY.get(player.getWorld()).HackerHasShop && GameWorldComponent.KEY.get(player.getWorld()).isRole(player, KinsWatheRoles.HACKER))) {
+               (ConfigWorldComponent.KEY.get(player.getWorld()).HackerHasShop && GameWorldComponent.KEY.get(player.getWorld()).isRole(player, KinsWatheRoles.HACKER))||
+                GameWorldComponent.KEY.get(player.getWorld()).isRole(player, KinsWatheRoles.ARBITER)) {
                 int balance = PlayerShopComponent.KEY.get(player).balance;
                 if (view.getTarget() != (float) balance) {
                     offsetDelta = (float) balance > view.getTarget() ? 0.6F : -0.6F;
