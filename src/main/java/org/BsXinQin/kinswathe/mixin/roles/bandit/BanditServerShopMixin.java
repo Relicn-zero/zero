ad9f88c093c57f3a525a3d5b443a3a9e2e5f052e
Relicn-zero <3373451267@qq.com>
2026-05-16 18:49:15 +0800

    Update BanditServerShopMixin.java

--- src/main/java/org/BsXinQin/kinswathe/mixin/roles/bandit/BanditServerShopMixin.java
+++ src/main/java/org/BsXinQin/kinswathe/mixin/roles/bandit/BanditServerShopMixin.java
@@ -1,12 +1,12 @@
 package org.BsXinQin.kinswathe.mixin.roles.bandit;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.doctor4t.wathe.cca.GameWorldComponent;
 import dev.doctor4t.wathe.cca.PlayerShopComponent;
 import dev.doctor4t.wathe.util.ShopEntry;
 import net.minecraft.entity.player.PlayerEntity;
 import org.BsXinQin.kinswathe.KinsWatheRoles;
 import org.BsXinQin.kinswathe.KinsWatheShops;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,25 +14,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(PlayerShopComponent.class)
-public abstract class BanditServerShopMixin {
+public class BanditServerShopMixin {
 
-    @Shadow @Final @NotNull private PlayerEntity player;
-    @Shadow private int balance;
-    @Shadow public abstract void sync();
+    @Shadow @Final private PlayerEntity player;
 
-    @Inject(method = "tryBuy", at = @At("HEAD"), cancellable = true)
-    void tryBuy(int index, @NotNull CallbackInfo ci) {
+    @Inject(method = "tryBuy", at = @At(value = "INVOKE", target = "Ljava/util/List;get(I)Ljava/lang/Object;", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void replaceShopEntriesForBuy(int index, CallbackInfo ci, @Local List<ShopEntry> entries) {
         GameWorldComponent gameWorld = GameWorldComponent.KEY.get(player.getWorld());
         if (gameWorld.isRole(player, KinsWatheRoles.BANDIT)) {
-            var entries = KinsWatheShops.getBanditShop(player.getWorld());
-            if (index < 0 || index >= entries.size()) return;
-            ShopEntry entry = entries.get(index);
-            if (KinsWatheShops.handlePurchase(player, this.balance, entry.stack().getItem(), entry.price())) {
-                this.balance -= entry.price();
-                this.sync();
-            }
-            ci.cancel();
+            List<ShopEntry> banditEntries = KinsWatheShops.getBanditShop(player.getWorld());
+            entries.clear();
+            entries.addAll(banditEntries);
         }
     }
 }
