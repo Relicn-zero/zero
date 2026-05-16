53977751f39a0e0f18e271e675421afa9287fa23
Relicn-zero <3373451267@qq.com>
2026-05-16 10:21:50 +0800

    Update KinsWatheConfig.java

--- src/main/java/org/BsXinQin/kinswathe/KinsWatheConfig.java
+++ src/main/java/org/BsXinQin/kinswathe/KinsWatheConfig.java
@@ -69,16 +69,16 @@ public class KinsWatheConfig {
     @SerialEntry
     public int CleanerAbilityCooldown = GameConstants.getInTicks(2,30) / 20;
 
-@SerialEntry(comment = "Arbiter: modify starting coins, price, glow duration, verdict delay and cooldown.")
+    @SerialEntry(comment = "Arbiter: starting coins, price, glow duration, verdict delay, cooldown.")
 public int ArbiterStartingCoins = 50;
 @SerialEntry
 public int ArbiterAbilityPrice = 200;
 @SerialEntry
 public int ArbiterGlowDuration = 10;
 @SerialEntry
-public int ArbiterVerdictDelay = 10;
+public int ArbiterVerdictDelay = 10;  // 发光后延迟死亡秒数
 @SerialEntry
-public int ArbiterCooldown = GameConstants.getInTicks(3,0) / 20;
+public int ArbiterCooldown = 180;     // 冷却时间（秒）
     
     @SerialEntry(comment = "Cook: modify price of Pan.")
     public int CookPanPrice = 250;
