ff91d4a533242da2ba49d52211ebe703f06b27b0
Relicn-zero <3373451267@qq.com>
2026-05-17 22:00:49 +0800

    Update BellringerAbility.java

--- src/main/java/org/BsXinQin/kinswathe/roles/bellringer/BellringerAbility.java
+++ src/main/java/org/BsXinQin/kinswathe/roles/bellringer/BellringerAbility.java
@@ -4,9 +4,12 @@ import dev.doctor4t.wathe.cca.GameTimeComponent;
 import dev.doctor4t.wathe.cca.GameWorldComponent;
 import dev.doctor4t.wathe.cca.PlayerShopComponent;
 import dev.doctor4t.wathe.game.GameFunctions;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import org.BsXinQin.kinswathe.KinsWatheConfig;
 import org.BsXinQin.kinswathe.KinsWatheRoles;
 import org.BsXinQin.kinswathe.component.AbilityPlayerComponent;
@@ -14,7 +17,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class BellringerAbility {
 
-    public static void register(@NotNull PlayerEntity player) {
+    public static void register(@NotNull ServerPlayerEntity player) {
         GameWorldComponent gameWorld = GameWorldComponent.KEY.get(player.getWorld());
         AbilityPlayerComponent ability = AbilityPlayerComponent.KEY.get(player);
         GameTimeComponent time = GameTimeComponent.KEY.get(player.getWorld());
@@ -25,6 +28,18 @@ public class BellringerAbility {
             playerShop.sync();
             time.setTime(Math.max(0, time.getTime() - 1200));
             player.playSoundToPlayer(SoundEvents.BLOCK_BELL_USE, SoundCategory.PLAYERS, 1.0f, 1.0f);
+
+            // 增强：给附近玩家速度II（可配置）
+            if (KinsWatheConfig.HANDLER.instance().EnableBellringerSpeedBoost) {
+                double range = 15.0;
+                for (ServerPlayerEntity p : player.getServer().getPlayerManager().getPlayerList()) {
+                    if (p.distanceTo(player) <= range && GameFunctions.isPlayerAliveAndSurvival(p)) {
+                        p.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 300, 1, false, true, true));
+                        p.sendMessage(Text.translatable("tip.kinswathe.bellringer.speed_boost"), true);
+                    }
+                }
+            }
+
             ability.setAbilityCooldown(KinsWatheConfig.HANDLER.instance().BellringerAbilityCooldown);
         }
     }
