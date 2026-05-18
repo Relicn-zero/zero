f08ada38a0b4ee8c319833e8fb59412b5c1fff97
Relicn-zero <3373451267@qq.com>
2026-05-18 17:53:13 +0800

    Update JudgeLordC2SPacket.java

--- src/main/java/org/BsXinQin/kinswathe/packet/roles/JudgeLordC2SPacket.java
+++ src/main/java/org/BsXinQin/kinswathe/packet/roles/JudgeLordC2SPacket.java
@@ -2,7 +2,6 @@ package org.BsXinQin.kinswathe.packet.roles;
 
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 import org.BsXinQin.kinswathe.KinsWathe;
@@ -13,9 +12,9 @@ import java.util.UUID;
 public record JudgeLordC2SPacket(UUID target) implements CustomPayload {
     public static final Identifier ID = Identifier.of(KinsWathe.MOD_ID, "judgelord");
     public static final Id<JudgeLordC2SPacket> PACKET_ID = new Id<>(ID);
-    public static final PacketCodec<PacketByteBuf, JudgeLordC2SPacket> CODEC = PacketCodec.tuple(
-        PacketCodecs.UUID, JudgeLordC2SPacket::target,
-        JudgeLordC2SPacket::new
+    public static final PacketCodec<PacketByteBuf, JudgeLordC2SPacket> CODEC = PacketCodec.of(
+        (packet, buf) -> buf.writeUuid(packet.target()),
+        buf -> new JudgeLordC2SPacket(buf.readUuid())
     );
 
     @Override
