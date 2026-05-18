78fcc018283eacbf07884a1a383530e938039835
Relicn-zero <3373451267@qq.com>
2026-05-18 19:53:48 +0800

    Update KobePickupMixin.java

--- src/main/java/org/BsXinQin/kinswathe/mixin/roles/kobe/KobePickupMixin.java
+++ src/main/java/org/BsXinQin/kinswathe/mixin/roles/kobe/KobePickupMixin.java
@@ -1,7 +1,6 @@
 package org.BsXinQin.kinswathe.mixin.roles.kobe;
 
 import dev.doctor4t.wathe.cca.GameWorldComponent;
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import org.BsXinQin.kinswathe.KinsWatheConfig;
@@ -11,12 +10,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ItemEntity.class)
+@Mixin(PlayerEntity.class)
 public class KobePickupMixin {
 
-    @Inject(method = "canMerge", at = @At("HEAD"), cancellable = true)
-    private void allowKobePickupVigilanteGun(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        // 此方法控制物品是否可以与实体合并，不适合拦截拾取。
-        // 更好的方法是监听 PlayerEntity 的拾取事件。
+    @Inject(method = "canPickupItem", at = @At("HEAD"), cancellable = true)
+    private void allowKobePickup(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        PlayerEntity self = (PlayerEntity) (Object) this;
+        GameWorldComponent gameWorld = GameWorldComponent.KEY.get(self.getWorld());
+        if (gameWorld.isRole(self, KinsWatheRoles.KOBE) && KinsWatheConfig.HANDLER.instance().KobeCanPickupVigilanteGun) {
+            // 允许拾取任何物品（覆盖原有限制）
+            cir.setReturnValue(true);
+        }
     }
 }
