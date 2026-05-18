766c0ee849a6b929994ee634be6b0ea9f91b8ba2
Relicn-zero <3373451267@qq.com>
2026-05-18 22:31:55 +0800

    Update KinsWatheRoles.java

--- src/main/java/org/BsXinQin/kinswathe/KinsWatheRoles.java
+++ src/main/java/org/BsXinQin/kinswathe/KinsWatheRoles.java
@@ -1,5 +1,6 @@
 package org.BsXinQin.kinswathe;
 
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import dev.doctor4t.wathe.api.Role;
@@ -65,36 +66,10 @@ public class KinsWatheRoles {
     public static Role PHYSICIAN = registerRole(new Role(Identifier.of(KinsWathe.MOD_ID, "physician"), 0xFFE5CC, true, false, Role.MoodType.REAL, WatheRoles.CIVILIAN.getMaxSprintTime() * 3 , false));
     public static Role ROBOT = registerRole(new Role(Identifier.of(KinsWathe.MOD_ID, "robot"), 0xC0C0C0, true, false, Role.MoodType.FAKE, -1, false));
     public static Role TECHNICIAN = registerRole(new Role(Identifier.of(KinsWathe.MOD_ID, "technician"), 0x003366, true, false, Role.MoodType.REAL, WatheRoles.CIVILIAN.getMaxSprintTime(), false));
-    public static Role BANDIT = registerRole(new Role(
-    Identifier.of(KinsWathe.MOD_ID, "bandit"),
-    0x8B4513,
-    false,   // 不是平民
-    true,    // 是杀手
-    Role.MoodType.FAKE,
-    -1,
-    true
-));
-    // 审判长 - 平民，通过点击头像发动裁决
-public static Role JUDGELORD = registerRole(new Role(
-    Identifier.of(KinsWathe.MOD_ID, "judgelord"),
-    0xAA66FF,   // 紫罗兰色
-    true,       // 平民
-    false,      // 不是杀手
-    Role.MoodType.REAL,
-    WatheRoles.CIVILIAN.getMaxSprintTime(),
-    false
-));
-    // 科比 - 中立角色，冲刺击退眩晕，拥有情绪（可获得情绪奖励）
-public static Role KOBE = registerRole(new Role(
-    Identifier.of(KinsWathe.MOD_ID, "kobe"),
-    0xFFD700,
-    true,      // 不是平民
-    false,      // 不是杀手
-    Role.MoodType.REAL,   // 改为 REAL，使科比拥有情绪系统
-    -1,
-    false
-));
-                                                      
+    public static Role BANDIT = registerRole(new Role(Identifier.of(KinsWathe.MOD_ID, "bandit"),0x8B4513, false, true, Role.MoodType.FAKE, -1, true));
+    public static Role JUDGELORD = registerRole(new Role(Identifier.of(KinsWathe.MOD_ID, "judgelord"),0xAA66FF, true, false, Role.MoodType.REAL, WatheRoles.CIVILIAN.getMaxSprintTime(), false));
+    public static Role KOBE = registerRole(new Role(Identifier.of(KinsWathe.MOD_ID, "kobe"),0xFFD700, true, false, Role.MoodType.REAL, WatheRoles.CIVILIAN.getMaxSprintTime(), false));
+
     /// 新增词条
     public static Modifier MAGNATE = registerModifier(new Modifier(Identifier.of(KinsWathe.MOD_ID, "magnate"), 0xFFFF00, null, new ArrayList<>(rolesHavePassiveIncome()), false, false));
     public static Modifier TASKMASTER = registerModifier(new Modifier(Identifier.of(KinsWathe.MOD_ID, "taskmaster"), 0xFF3399, null, new ArrayList<>(rolesHaveTaskIncome()), false, false));
@@ -120,44 +95,44 @@ public static Role KOBE = registerRole(new Role(
     /// 收入身份列表
     public static List<Role> rolesHaveTaskIncome() {
         List<Role> roles = new ArrayList<>();
-        roles.add(WatheRoles.KILLER); roles.add(BELLRINGER);roles.add(ARBITER);roles.add(KOBE);roles.add(JUDGELORD);roles.add(BODYMAKER); roles.add(CLEANER); roles.add(COOK); roles.add(DETECTIVE); roles.add(DRUGMAKER); roles.add(HUNTER); roles.add(JUDGE); roles.add(KIDNAPPER); roles.add(LICENSED_VILLAIN); roles.add(PHYSICIAN); roles.add(TECHNICIAN);
+        roles.add(WatheRoles.KILLER); roles.add(BELLRINGER); roles.add(ARBITER); roles.add(KOBE); roles.add(JUDGELORD); roles.add(BODYMAKER); roles.add(CLEANER); roles.add(COOK); roles.add(DETECTIVE); roles.add(DRUGMAKER); roles.add(HUNTER); roles.add(JUDGE); roles.add(KIDNAPPER); roles.add(LICENSED_VILLAIN); roles.add(PHYSICIAN); roles.add(TECHNICIAN);
         if (KinsWatheConfig.HANDLER.instance().HackerHasShop) roles.add(HACKER);
         if (FabricLoader.getInstance().isModLoaded("noellesroles")) { roles.add(noellesrolesRoles("PHANTOM")); roles.add(noellesrolesRoles("SWAPPER")); roles.add(noellesrolesRoles("TRAPPER")); roles.add(noellesrolesRoles("RECALLER")); roles.add(noellesrolesRoles("BARTENDER")); roles.add(noellesrolesRoles("MORPHLING")); roles.add(noellesrolesRoles("NOISEMAKER")); roles.add(noellesrolesRoles("THE_INSANE_DAMNED_PARANOID_KILLER_OF_DOOM_DEATH_DESTRUCTION_AND_WAFFLES")); }
         return List.copyOf(roles);
     }
     public static List<Role> rolesHavePassiveIncome() {
-    List<Role> roles = new ArrayList<>();
-    roles.add(WatheRoles.KILLER);
-    roles.add(BODYMAKER);
-    roles.add(ARBITER);
-    roles.add(JUDGELORD);
-    roles.add(KOBE);
-    roles.add(CLEANER);
-    roles.add(COOK);
-    roles.add(DREAMER);
-    roles.add(DRUGMAKER);
-    roles.add(HUNTER);
-    roles.add(JUDGE);
-    roles.add(KIDNAPPER);
-    if (KinsWatheConfig.HANDLER.instance().HackerHasShop) roles.add(HACKER);
-    if (FabricLoader.getInstance().isModLoaded("noellesroles")) {
-        roles.add(noellesrolesRoles("MIMIC"));
-        roles.add(noellesrolesRoles("PHANTOM"));
-        roles.add(noellesrolesRoles("SWAPPER"));
-        roles.add(noellesrolesRoles("MORPHLING"));
-        roles.add(noellesrolesRoles("NOISEMAKER"));
-        roles.add(noellesrolesRoles("EXECUTIONER"));
-        roles.add(noellesrolesRoles("THE_INSANE_DAMNED_PARANOID_KILLER_OF_DOOM_DEATH_DESTRUCTION_AND_WAFFLES"));
-        if (noellesrolesRoles("JESTER") != null) roles.add(noellesrolesRoles("JESTER"));
+        List<Role> roles = new ArrayList<>();
+        roles.add(WatheRoles.KILLER);
+        roles.add(BODYMAKER);
+        roles.add(ARBITER);
+        roles.add(JUDGELORD);
+        roles.add(KOBE);
+        roles.add(CLEANER);
+        roles.add(COOK);
+        roles.add(DREAMER);
+        roles.add(DRUGMAKER);
+        roles.add(HUNTER);
+        roles.add(JUDGE);
+        roles.add(KIDNAPPER);
+        if (KinsWatheConfig.HANDLER.instance().HackerHasShop) roles.add(HACKER);
+        if (FabricLoader.getInstance().isModLoaded("noellesroles")) {
+            roles.add(noellesrolesRoles("MIMIC"));
+            roles.add(noellesrolesRoles("PHANTOM"));
+            roles.add(noellesrolesRoles("SWAPPER"));
+            roles.add(noellesrolesRoles("MORPHLING"));
+            roles.add(noellesrolesRoles("NOISEMAKER"));
+            roles.add(noellesrolesRoles("EXECUTIONER"));
+            roles.add(noellesrolesRoles("THE_INSANE_DAMNED_PARANOID_KILLER_OF_DOOM_DEATH_DESTRUCTION_AND_WAFFLES"));
+            if (noellesrolesRoles("JESTER") != null) roles.add(noellesrolesRoles("JESTER"));
+        }
+        return List.copyOf(roles);
     }
-    return List.copyOf(roles);
-}
 
     /// 阵营
     public static final ArrayList<Role> NEUTRAL_ROLES = new ArrayList<>();
     public static final ArrayList<Role> KILLER_NEUTRAL_ROLES = new ArrayList<>();
     public static void addNewRoleCamps() { addNeutralRoles(); addKillerNeutralRoles(); }
-    public static void addNeutralRoles() { NEUTRAL_ROLES.add(LICENSED_VILLAIN); NEUTRAL_ROLES.add(KOBE);}
+    public static void addNeutralRoles() { NEUTRAL_ROLES.add(LICENSED_VILLAIN); }
     public static void addKillerNeutralRoles() { KILLER_NEUTRAL_ROLES.add(HACKER); if (FabricLoader.getInstance().isModLoaded("noellesroles")) { KILLER_NEUTRAL_ROLES.add(DREAMER); Harpymodloader.setRoleMaximum(DREAMER, 1); } }
 
     /// 限制人数
@@ -175,56 +150,59 @@ public static Role KOBE = registerRole(new Role(
         return (role1 == HACKER && role2 == noellesrolesRoles("MIMIC")) || (role1 == noellesrolesRoles("MIMIC") && role2 == HACKER);
     }
 
-/// 初始事件
-public static void setDefaultEvents() {
-    ModdedRoleAssigned.EVENT.register((player, role) -> {
-        AbilityPlayerComponent ability = AbilityPlayerComponent.KEY.get(player);
-        ability.cooldown = KinsWatheConfig.HANDLER.instance().StartingCooldown * 20;
-        GameWorldComponent gameWorld = GameWorldComponent.KEY.get(player.getWorld());
-        PlayerShopComponent playerShop = PlayerShopComponent.KEY.get(player);
-        DreamerKillerComponent playerDreamer = DreamerKillerComponent.KEY.get(player);
-        HackerPhoneComponent phone = HackerPhoneComponent.KEY.get(player);
+    /// 初始事件
+    public static void setDefaultEvents() {
+        ModdedRoleAssigned.EVENT.register((player, role) -> {
+            AbilityPlayerComponent ability = AbilityPlayerComponent.KEY.get(player);
+            ability.cooldown = KinsWatheConfig.HANDLER.instance().StartingCooldown * 20;
+            GameWorldComponent gameWorld = GameWorldComponent.KEY.get(player.getWorld());
+            PlayerShopComponent playerShop = PlayerShopComponent.KEY.get(player);
+            DreamerKillerComponent playerDreamer = DreamerKillerComponent.KEY.get(player);
+            HackerPhoneComponent phone = HackerPhoneComponent.KEY.get(player);
 
-        // 阵营初始收入
-        if (KinsWatheConfig.HANDLER.instance().EnableWatheModify) {
-            if (gameWorld.isInnocent(player)) 
-                playerShop.addToBalance(KinsWatheConfig.HANDLER.instance().InitialCivilianIncome);
-            if (!gameWorld.isInnocent(player) && !gameWorld.canUseKillerFeatures(player)) 
-                playerShop.addToBalance(KinsWatheConfig.HANDLER.instance().InitialNeutralIncome);
-            if (gameWorld.canUseKillerFeatures(player)) 
-                playerShop.addToBalance(KinsWatheConfig.HANDLER.instance().InitialKillerIncome - 100);
-        }
+            // 阵营初始收入
+            if (KinsWatheConfig.HANDLER.instance().EnableWatheModify) {
+                if (gameWorld.isInnocent(player)) 
+                    playerShop.addToBalance(KinsWatheConfig.HANDLER.instance().InitialCivilianIncome);
+                if (!gameWorld.isInnocent(player) && !gameWorld.canUseKillerFeatures(player)) 
+                    playerShop.addToBalance(KinsWatheConfig.HANDLER.instance().InitialNeutralIncome);
+                if (gameWorld.canUseKillerFeatures(player)) 
+                    playerShop.addToBalance(KinsWatheConfig.HANDLER.instance().InitialKillerIncome - 100);
+            }
 
-        // 各角色初始物品
-        if (role.equals(CLEANER)) 
-            player.giveItemStack(KinsWatheItems.SULFURIC_ACID_BARREL.getDefaultStack());
-        if (role.equals(DREAMER)) { 
-            player.giveItemStack(new ItemStack(KinsWatheItems.DREAM_IMPRINT, KinsWatheConfig.HANDLER.instance().DreamerInitialItemQuantity)); 
-            playerDreamer.setDreamerRequired(); 
-        }
-        if (role.equals(HACKER)) {
-            player.giveItemStack(phone.getPhone());
-            for (ServerPlayerEntity serverPlayer : player.getServer().getPlayerManager().getPlayerList()) {
-                if (serverPlayer != null && gameWorld.canUseKillerFeatures(serverPlayer)) 
-                    serverPlayer.giveItemStack(phone.getPhone());
+            // 各角色初始物品/特殊效果
+            if (role.equals(CLEANER)) 
+                player.giveItemStack(KinsWatheItems.SULFURIC_ACID_BARREL.getDefaultStack());
+            if (role.equals(DREAMER)) { 
+                player.giveItemStack(new ItemStack(KinsWatheItems.DREAM_IMPRINT, KinsWatheConfig.HANDLER.instance().DreamerInitialItemQuantity)); 
+                playerDreamer.setDreamerRequired(); 
             }
-        }
-        if (role.equals(KIDNAPPER)) 
-            player.giveItemStack(KinsWatheItems.KNOCKOUT_DRUG.getDefaultStack());
-        if (role.equals(LICENSED_VILLAIN)) 
-            player.giveItemStack(WatheItems.LOCKPICK.getDefaultStack());
-        if (role.equals(PHYSICIAN)) 
-            player.giveItemStack(KinsWatheItems.MEDICAL_KIT.getDefaultStack());
-        if (role.equals(ARBITER)) { 
-            /* 初始物品留空 */ 
-        }
-        if (role.equals(ROBOT)) {
-            if (KinsWatheConfig.HANDLER.instance().EnableRobotSpeedEffect) {
-                player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, Integer.MAX_VALUE, 0, false, false, true));
+            if (role.equals(HACKER)) {
+                player.giveItemStack(phone.getPhone());
+                for (ServerPlayerEntity serverPlayer : player.getServer().getPlayerManager().getPlayerList()) {
+                    if (serverPlayer != null && gameWorld.canUseKillerFeatures(serverPlayer)) 
+                        serverPlayer.giveItemStack(phone.getPhone());
+                }
             }
-        }
-    });
-}
+            if (role.equals(KIDNAPPER)) 
+                player.giveItemStack(KinsWatheItems.KNOCKOUT_DRUG.getDefaultStack());
+            if (role.equals(LICENSED_VILLAIN)) 
+                player.giveItemStack(WatheItems.LOCKPICK.getDefaultStack());
+            if (role.equals(PHYSICIAN)) 
+                player.giveItemStack(KinsWatheItems.MEDICAL_KIT.getDefaultStack());
+            if (role.equals(ARBITER)) { 
+                /* 初始物品留空 */ 
+            }
+            
+            // 机器人：永久速度加成（修改基础移动速度属性）
+            if (role.equals(ROBOT)) {
+                player.removeStatusEffect(StatusEffects.SPEED); // 清除可能的速度状态
+                double originalSpeed = player.getAttributeInstance(EntityAttributes.MOVEMENT_SPEED).getBaseValue();
+                double boostedSpeed = originalSpeed * 1.2; // 20% 速度加成，可调整
+                player.getAttributeInstance(EntityAttributes.MOVEMENT_SPEED).setBaseValue(boostedSpeed);
+            }
+        });
+    }
 
     /// 注册身份技能
     public static void registerRolesAbility() {
@@ -234,7 +212,7 @@ public static void setDefaultEvents() {
             DetectiveAbility.register(context.player());
             HunterAbility.register(context.player());
             RobotAbility.register(context.player());
-            ArbiterAbility.register(context.player());   // 裁决者使用通用能力键
+            ArbiterAbility.register(context.player());
             KobeAbility.register(context.player());
         });
         ServerPlayNetworking.registerGlobalReceiver(BodymakerC2SPacket.ID, (payload, context) -> BodymakerAbility.register(payload, context.player()));
