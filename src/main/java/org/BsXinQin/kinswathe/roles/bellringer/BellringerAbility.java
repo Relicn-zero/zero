641a3da92894ce1904577b17ccaa040fe5e9c46b
Relicn-zero <3373451267@qq.com>
2026-05-19 19:45:37 +0800

    Update BellringerAbility.java

--- src/main/java/org/BsXinQin/kinswathe/roles/bellringer/BellringerAbility.java
+++ src/main/java/org/BsXinQin/kinswathe/roles/bellringer/BellringerAbility.java
@@ -14,26 +14,34 @@ import org.BsXinQin.kinswathe.component.TempSpeedComponent;
 import org.jetbrains.annotations.NotNull;
 
 public class BellringerAbility {
-
     public static void register(@NotNull PlayerEntity player) {
         GameWorldComponent gameWorld = GameWorldComponent.KEY.get(player.getWorld());
         AbilityPlayerComponent ability = AbilityPlayerComponent.KEY.get(player);
         GameTimeComponent time = GameTimeComponent.KEY.get(player.getWorld());
         PlayerShopComponent playerShop = PlayerShopComponent.KEY.get(player);
 
-        if (gameWorld.isRole(player, KinsWatheRoles.BELLRINGER) && GameFunctions.isPlayerAliveAndSurvival(player) && ability.cooldown <= 0) {
+        if (gameWorld.isRole(player, KinsWatheRoles.BELLRINGER)
+                && GameFunctions.isPlayerAliveAndSurvival(player)
+                && ability.cooldown <= 0) {
+
+            // 金币检查
             if (playerShop.balance < KinsWatheConfig.HANDLER.instance().BellringerAbilityPrice) return;
 
+            // 扣钱
             playerShop.balance -= KinsWatheConfig.HANDLER.instance().BellringerAbilityPrice;
             playerShop.sync();
 
+            // 核心效果：减少1分钟（1200刻）剩余时间
             time.setTime(Math.max(0, time.getTime() - 1200));
 
+            // 音效
             player.playSoundToPlayer(SoundEvents.BLOCK_BELL_USE, SoundCategory.PLAYERS, 1.0f, 1.0f);
 
+            // 速度加成
             int durationSec = KinsWatheConfig.HANDLER.instance().BellringerSpeedDuration;
             TempSpeedComponent.KEY.get(player).activate(durationSec * 20);
 
+            // 设置冷却
             ability.setAbilityCooldown(KinsWatheConfig.HANDLER.instance().BellringerAbilityCooldown);
         }
     }
