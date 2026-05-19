3c3623a99d44880046aacff42571570331132eca
Relicn-zero <3373451267@qq.com>
2026-05-19 19:02:19 +0800

    Update KobeSpeedMixin.java

--- src/main/java/org/BsXinQin/kinswathe/mixin/roles/kobe/KobeSpeedMixin.java
+++ src/main/java/org/BsXinQin/kinswathe/mixin/roles/kobe/KobeSpeedMixin.java
@@ -1,10 +1,10 @@
-package org.BsXinQin.kinswathe.mixin.roles.kobe;
+package org.BsXinQin.kinswathe.mixin.host;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
 import org.BsXinQin.kinswathe.KinsWatheConfig;
 import org.BsXinQin.kinswathe.KinsWatheRoles;
+import org.BsXinQin.kinswathe.component.TempSpeedComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -14,11 +14,14 @@ public class KobeSpeedMixin {
     @ModifyReturnValue(method = "getMovementSpeed", at = @At("RETURN"))
     private float modifyKobeSpeed(float original) {
         PlayerEntity self = (PlayerEntity) (Object) this;
-        // 判断是否为科比且技能激活（需要用一个组件存储激活状态）
-        // 这里简化：假设技能激活时某组件 ticks > 0
-        if (KinsWatheRoles.isKobe(self) && KobeComponent.hasActive(self)) {
-            return self.isSprinting() ? (float) KinsWatheConfig.HANDLER.instance().KobeSprintSpeed : (float) KinsWatheConfig.HANDLER.instance().KobeWalkSpeed;
-        }
+        try {
+            if (dev.doctor4t.wathe.cca.GameWorldComponent.KEY.get(self.getWorld()).isRole(self, KinsWatheRoles.KOBE)) {
+                TempSpeedComponent comp = TempSpeedComponent.KEY.get(self);
+                if (comp.isActive()) {
+                    return self.isSprinting() ? (float) KinsWatheConfig.HANDLER.instance().KobeCustomSprintSpeed : (float) KinsWatheConfig.HANDLER.instance().KobeCustomWalkSpeed;
+                }
+            }
+        } catch (Exception ignored) {}
         return original;
     }
 }
