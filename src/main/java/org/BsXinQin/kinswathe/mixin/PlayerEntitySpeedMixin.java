01a70e67658ce997776a836190f48a78ef9a8574
Relicn-zero <3373451267@qq.com>
2026-05-19 19:34:21 +0800

    Update PlayerEntitySpeedMixin.java

--- src/main/java/org/BsXinQin/kinswathe/mixin/PlayerEntitySpeedMixin.java
+++ src/main/java/org/BsXinQin/kinswathe/mixin/PlayerEntitySpeedMixin.java
@@ -1,5 +1,6 @@
 package org.BsXinQin.kinswathe.mixin;
 
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import org.BsXinQin.kinswathe.component.SpeedComponent;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,13 +10,19 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(PlayerEntity.class)
 public class PlayerEntitySpeedMixin {
 
-    @ModifyVariable(method = "getMovementSpeed", at = @At("RETURN"), ordinal = 0)
-    private float modifySpeed(float originalSpeed) {
+    @ModifyVariable(
+        method = "tickMovement",
+        at = @At("HEAD"),
+        argsOnly = true
+    )
+    private void applySpeedModifier() {
+        // 在 tickMovement 开始时修改移动速度属性
         PlayerEntity player = (PlayerEntity) (Object) this;
         SpeedComponent speedComp = SpeedComponent.KEY.get(player);
         if (speedComp != null) {
-            return originalSpeed * speedComp.getFinalMultiplier();
+            float multiplier = speedComp.getFinalMultiplier();
+            player.getAttributeInstance(EntityAttributes.MOVEMENT_SPEED)
+                    .setBaseValue(0.1f * multiplier); // 原版基础速度 0.1
         }
-        return originalSpeed;
     }
 }
