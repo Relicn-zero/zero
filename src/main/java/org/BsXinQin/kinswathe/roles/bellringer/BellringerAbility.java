73f2dfc1ba4c3efe638a2b86071ac45640331b29
Relicn-zero <3373451267@qq.com>
2026-05-19 18:06:21 +0800

    Update BellringerAbility.java

--- src/main/java/org/BsXinQin/kinswathe/roles/bellringer/BellringerAbility.java
+++ src/main/java/org/BsXinQin/kinswathe/roles/bellringer/BellringerAbility.java
@@ -24,23 +24,17 @@ public class BellringerAbility {
         if (gameWorld.isRole(player, KinsWatheRoles.BELLRINGER) && GameFunctions.isPlayerAliveAndSurvival(player) && ability.cooldown <= 0) {
             if (playerShop.balance < KinsWatheConfig.HANDLER.instance().BellringerAbilityPrice) return;
 
-            // 扣除金币
             playerShop.balance -= KinsWatheConfig.HANDLER.instance().BellringerAbilityPrice;
             playerShop.sync();
 
-            // 减少游戏时间 1 分钟（1200 tick）
             time.setTime(Math.max(0, time.getTime() - 1200));
 
-            // 播放音效
             player.playSoundToPlayer(SoundEvents.BLOCK_BELL_USE, SoundCategory.PLAYERS, 1.0f, 1.0f);
 
-            // 临时速度加成
             int durationSec = KinsWatheConfig.HANDLER.instance().BellringerSpeedDuration;
             float multiplier = (float) KinsWatheConfig.HANDLER.instance().BellringerSpeedMultiplier;
-            int durationTicks = durationSec * 20;
-            SpeedComponent.KEY.get(player).setTemporaryMultiplier(multiplier, durationTicks);
+            SpeedComponent.KEY.get(player).setMultiplier(multiplier, durationSec * 20);
 
-            // 设置技能冷却
             ability.setAbilityCooldown(KinsWatheConfig.HANDLER.instance().BellringerAbilityCooldown);
         }
     }
