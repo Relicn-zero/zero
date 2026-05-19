866bf8940c9ae6e0957dcb0630d8b3c1b0018ad7
Relicn-zero <3373451267@qq.com>
2026-05-19 17:27:29 +0800

    Update BellringerHudMixin.java

--- src/client/java/org/BsXinQin/kinswathe/client/mixin/roles/bellringer/BellringerHudMixin.java
+++ src/client/java/org/BsXinQin/kinswathe/client/mixin/roles/bellringer/BellringerHudMixin.java
@@ -9,11 +9,10 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.text.Text;
+import org.BsXinQin.kinswathe.KinsWatheConfig;
 import org.BsXinQin.kinswathe.KinsWatheRoles;
 import org.BsXinQin.kinswathe.client.KinsWatheInitializeClient;
 import org.BsXinQin.kinswathe.component.AbilityPlayerComponent;
-import org.BsXinQin.kinswathe.component.ConfigWorldComponent;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -26,23 +25,29 @@ public abstract class BellringerHudMixin {
     @Shadow public abstract TextRenderer getTextRenderer();
 
     @Inject(method = "render", at = @At("TAIL"))
-    public void getAbilityHud(@NotNull DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-        if (MinecraftClient.getInstance().player == null) return;
-        GameWorldComponent gameWorld = GameWorldComponent.KEY.get(MinecraftClient.getInstance().player.getWorld());
-        AbilityPlayerComponent ability = AbilityPlayerComponent.KEY.get(MinecraftClient.getInstance().player);
-        PlayerShopComponent playerShop = PlayerShopComponent.KEY.get(MinecraftClient.getInstance().player);
-        if (gameWorld.isRole(MinecraftClient.getInstance().player, KinsWatheRoles.BELLRINGER) && WatheClient.isPlayerAliveAndInSurvival()) {
+    public void getAbilityHud(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        var player = MinecraftClient.getInstance().player;
+        if (player == null) return;
+
+        var gameWorld = GameWorldComponent.KEY.get(player.getWorld());
+        var ability = AbilityPlayerComponent.KEY.get(player);
+        var playerShop = PlayerShopComponent.KEY.get(player);
+
+        if (gameWorld.isRole(player, KinsWatheRoles.BELLRINGER) && WatheClient.isPlayerAliveAndInSurvival()) {
             int drawY = context.getScaledWindowHeight();
             Text line;
-            if (playerShop.balance < ConfigWorldComponent.KEY.get(MinecraftClient.getInstance().player.getWorld()).BellringerAbilityPrice) {
-                line = Text.translatable("tip.kinswathe.ability.not_enough_money", ConfigWorldComponent.KEY.get(MinecraftClient.getInstance().player.getWorld()).BellringerAbilityPrice);
+            int price = KinsWatheConfig.HANDLER.instance().BellringerAbilityPrice;
+            if (playerShop.balance < price) {
+                line = Text.translatable("tip.kinswathe.ability.not_enough_money", price);
             } else if (ability.cooldown > 0) {
                 line = Text.translatable("tip.kinswathe.cooldown", ability.cooldown / 20);
             } else {
                 line = Text.translatable("tip.kinswathe.ability.can_use", KinsWatheInitializeClient.abilityBind.getBoundKeyLocalizedText());
             }
             drawY -= getTextRenderer().getWrappedLinesHeight(line, 999999);
-            context.drawTextWithShadow(getTextRenderer(), line, context.getScaledWindowWidth() - getTextRenderer().getWidth(line), drawY, KinsWatheRoles.BELLRINGER.color());
+            context.drawTextWithShadow(getTextRenderer(), line,
+                context.getScaledWindowWidth() - getTextRenderer().getWidth(line),
+                drawY, KinsWatheRoles.BELLRINGER.color());
         }
     }
 }
