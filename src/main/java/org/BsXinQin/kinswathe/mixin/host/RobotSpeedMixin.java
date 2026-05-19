d6243ed49529563191a4482e6767d054a6ed1449
Relicn-zero <3373451267@qq.com>
2026-05-19 19:02:04 +0800

    Update RobotSpeedMixin.java

--- src/main/java/org/BsXinQin/kinswathe/mixin/host/RobotSpeedMixin.java
+++ src/main/java/org/BsXinQin/kinswathe/mixin/host/RobotSpeedMixin.java
@@ -2,7 +2,6 @@ package org.BsXinQin.kinswathe.mixin.host;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.player.PlayerEntity;
-import org.BsXinQin.kinswathe.KinsWatheConfig;
 import org.BsXinQin.kinswathe.KinsWatheRoles;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,16 +12,12 @@ public class RobotSpeedMixin {
     @ModifyReturnValue(method = "getMovementSpeed", at = @At("RETURN"))
     private float modifyRobotSpeed(float original) {
         PlayerEntity self = (PlayerEntity) (Object) this;
-        // 注意：需要判断是否为机器人，且游戏正在进行中（可选）
-        if (KinsWatheRoles.getRoles().containsValue(KinsWatheRoles.ROBOT) && self.getWorld() != null) {
-            // 更严格的判断：检查角色
-            try {
-                if (dev.doctor4t.wathe.cca.GameWorldComponent.KEY.get(self.getWorld()).isRole(self, KinsWatheRoles.ROBOT)) {
-                    float multiplier = (float) KinsWatheConfig.HANDLER.instance().RobotSpeedMultiplier;
-                    return original * multiplier;
-                }
-            } catch (Exception ignored) {}
-        }
+        // 判断是否为机器人
+        try {
+            if (dev.doctor4t.wathe.cca.GameWorldComponent.KEY.get(self.getWorld()).isRole(self, KinsWatheRoles.ROBOT)) {
+                return 0.12f; // 固定速度值，可配置
+            }
+        } catch (Exception ignored) {}
         return original;
     }
 }
