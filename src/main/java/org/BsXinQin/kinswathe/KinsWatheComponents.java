fd4d7db569c7bb3414dd12d7bc417bee820b2056
Relicn-zero <3373451267@qq.com>
2026-05-19 17:51:38 +0800

    Update KinsWatheComponents.java

--- src/main/java/org/BsXinQin/kinswathe/KinsWatheComponents.java
+++ src/main/java/org/BsXinQin/kinswathe/KinsWatheComponents.java
@@ -41,7 +41,7 @@ public class KinsWatheComponents implements EntityComponentInitializer, WorldCom
         registry.beginRegistration(PlayerEntity.class, JudgeLordComponent.KEY)
     .respawnStrategy(RespawnCopyStrategy.NEVER_COPY)
     .end(JudgeLordComponent::new);
-        registry.beginRegistration(PlayerEntity.class, SpeedComponent.KEY)
+        rregistry.beginRegistration(PlayerEntity.class, SpeedComponent.KEY)
         .respawnStrategy(RespawnCopyStrategy.NEVER_COPY)
         .end(SpeedComponent::new);
     }
