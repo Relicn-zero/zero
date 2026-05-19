f3cd4d002331b96b4a4b015fab00afaea0deb95e
Relicn-zero <3373451267@qq.com>
2026-05-19 19:11:07 +0800

    Update RobotSpeedMixin.java

--- src/main/java/org/BsXinQin/kinswathe/mixin/host/RobotSpeedMixin.java
+++ src/main/java/org/BsXinQin/kinswathe/mixin/host/RobotSpeedMixin.java
@@ -2,6 +2,7 @@ package org.BsXinQin.kinswathe.mixin.host;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.player.PlayerEntity;
+import org.BsXinQin.kinswathe.KinsWatheConfig;
 import org.BsXinQin.kinswathe.KinsWatheRoles;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,10 +13,10 @@ public class RobotSpeedMixin {
     @ModifyReturnValue(method = "getMovementSpeed", at = @At("RETURN"))
     private float modifyRobotSpeed(float original) {
         PlayerEntity self = (PlayerEntity) (Object) this;
-        // 判断是否为机器人
         try {
             if (dev.doctor4t.wathe.cca.GameWorldComponent.KEY.get(self.getWorld()).isRole(self, KinsWatheRoles.ROBOT)) {
-                return 0.12f; // 固定速度值，可配置
+                float multiplier = (float) KinsWatheConfig.HANDLER.instance().RobotSpeedMultiplier;
+                return original * multiplier;
             }
         } catch (Exception ignored) {}
         return original;
