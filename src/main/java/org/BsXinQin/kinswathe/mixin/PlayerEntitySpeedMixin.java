86df332cf58193248ca9947ef336fc73e20dd6a6
Relicn-zero <3373451267@qq.com>
2026-05-19 19:18:34 +0800

    Create PlayerEntitySpeedMixin.java

---
+++ src/main/java/org/BsXinQin/kinswathe/mixin/PlayerEntitySpeedMixin.java
@@ -0,0 +1,22 @@
+package org.BsXinQin.kinswathe.mixin;
+
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
+import org.BsXinQin.kinswathe.component.SpeedComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntitySpeedMixin {
+
+    @ModifyVariable(method = "getMovementSpeed", at = @At("RETURN"), ordinal = 0)
+    private float modifySpeed(float originalSpeed) {
+        PlayerEntity player = (PlayerEntity) (Object) this;
+        SpeedComponent speedComp = SpeedComponent.KEY.get(player);
+        if (speedComp != null) {
+            return originalSpeed * speedComp.getFinalMultiplier();
+        }
+        return originalSpeed;
+    }
+}
