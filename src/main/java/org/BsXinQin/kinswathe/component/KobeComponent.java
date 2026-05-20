49358aad7dd0b766acd9323225728f0b1c813764
Relicn-zero <3373451267@qq.com>
2026-05-20 17:15:27 +0800

    Update KobeComponent.java

--- src/main/java/org/BsXinQin/kinswathe/component/KobeComponent.java
+++ src/main/java/org/BsXinQin/kinswathe/component/KobeComponent.java
@@ -1,15 +1,24 @@
 package org.BsXinQin.kinswathe.component;
 
-// ... (必要的 import 语句)
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
+import org.BsXinQin.kinswathe.KinsWathe;
+import org.ladysnake.cca.api.v3.component.ComponentKey;
+import org.ladysnake.cca.api.v3.component.ComponentRegistry;
+import org.ladysnake.cca.api.v3.component.sync.AutoSyncedComponent;
 import org.ladysnake.cca.api.v3.component.tick.ServerTickingComponent;
-// ...
 
 public class KobeComponent implements AutoSyncedComponent, ServerTickingComponent {
     public static final ComponentKey<KobeComponent> KEY = ComponentRegistry.getOrCreate(
-        Identifier.of(KinsWathe.MOD_ID, "kobe"), KobeComponent.class
+            Identifier.of(KinsWathe.MOD_ID, "kobe"), KobeComponent.class
     );
+
     private final PlayerEntity player;
-    public int speedTicks = 0; // 用于技能速度的倒计时
+    public int speedTicks = 0;
 
     public KobeComponent(PlayerEntity player) {
         this.player = player;
@@ -18,23 +27,39 @@ public class KobeComponent implements AutoSyncedComponent, ServerTickingComponen
     @Override
     public void serverTick() {
         if (this.speedTicks > 0) {
-            this.speedTicks--; // 每 tick 减 1
+            this.speedTicks--;
             if (player instanceof ServerPlayerEntity serverPlayer) {
-                // 技能激活时持续产生特效
                 serverPlayer.getServerWorld().spawnParticles(
-                    KinsWathe.SWEEP_ATTACK, // 你可以替换成你想要的粒子效果
-                    serverPlayer.getX(), serverPlayer.getY() + 0.5, serverPlayer.getZ(),
-                    1, 0.2, 0.2, 0.2, 0
+                        ParticleTypes.SWEEP_ATTACK,
+                        serverPlayer.getX(), serverPlayer.getY() + 0.5, serverPlayer.getZ(),
+                        1, 0.2, 0.2, 0.2, 0
                 );
             }
-            this.sync(); // 同步数据到客户端
+            this.sync();
         }
     }
 
-    // 辅助方法
-    public void setSpeedTicks(int ticks) { this.speedTicks = ticks; this.sync(); }
-    public void reset() { this.speedTicks = 0; this.sync(); }
-    public void sync() { KEY.sync(this.player); }
+    public void setSpeedTicks(int ticks) {
+        this.speedTicks = ticks;
+        this.sync();
+    }
 
-    // writeToNbt / readFromNbt 方法保持不变
+    public void reset() {
+        this.speedTicks = 0;
+        this.sync();
+    }
+
+    public void sync() {
+        KEY.sync(this.player);
+    }
+
+    @Override
+    public void writeToNbt(NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
+        tag.putInt("speedTicks", this.speedTicks);
+    }
+
+    @Override
+    public void readFromNbt(NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
+        this.speedTicks = tag.getInt("speedTicks");
+    }
 }
