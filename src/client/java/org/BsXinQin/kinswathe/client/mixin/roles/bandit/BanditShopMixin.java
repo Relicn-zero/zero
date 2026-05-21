f06a1d05a088e5141267c9d2764f52ed810b0a1a
Relicn-zero <3373451267@qq.com>
2026-05-21 19:05:58 +0800

    Update BanditShopMixin.java

--- src/client/java/org/BsXinQin/kinswathe/client/mixin/roles/bandit/BanditShopMixin.java
+++ src/client/java/org/BsXinQin/kinswathe/client/mixin/roles/bandit/BanditShopMixin.java
@@ -6,6 +6,7 @@ import dev.doctor4t.wathe.util.ShopEntry;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.BsXinQin.kinswathe.KinsWatheRoles;
 import org.BsXinQin.kinswathe.KinsWatheShops;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,16 +16,16 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import java.util.List;
 
 @Mixin(LimitedInventoryScreen.class)
-public class BanditShopMixin {
+public abstract class BanditShopMixin {
 
-    @Shadow @Final public ClientPlayerEntity player;
+    @Shadow @Final @NotNull public ClientPlayerEntity player;
 
     @ModifyVariable(method = "init", at = @At(value = "STORE"), name = "entries")
-    private List<ShopEntry> getBanditShop(List<ShopEntry> originalEntries) {
-        GameWorldComponent gameWorld = GameWorldComponent.KEY.get(player.getWorld());
-        if (gameWorld.isRole(player, KinsWatheRoles.BANDIT)) {
-            return KinsWatheShops.getBanditShop(player.getWorld());
+    private List<ShopEntry> getBanditShop(@NotNull List<ShopEntry> entries) {
+        GameWorldComponent gameWorld = GameWorldComponent.KEY.get(this.player.getWorld());
+        if (gameWorld.isRole(this.player, KinsWatheRoles.BANDIT)) {
+            entries = KinsWatheShops.getBanditShop(this.player.getWorld());
         }
-        return originalEntries;
+        return entries;
     }
 }
