56fc95f8e26895f1f4061e296ce6150047c8d2bc
Relicn-zero <3373451267@qq.com>
2026-05-21 19:58:23 +0800

    Update BanditShopMixin.java

--- src/main/java/org/BsXinQin/kinswathe/mixin/roles/bandit/BanditShopMixin.java
+++ src/main/java/org/BsXinQin/kinswathe/mixin/roles/bandit/BanditShopMixin.java
@@ -1,30 +1,40 @@
-package org.BsXinQin.kinswathe.client.mixin.roles.bandit;
+package org.BsXinQin.kinswathe.mixin.roles.bandit;
 
 import dev.doctor4t.wathe.cca.GameWorldComponent;
-import dev.doctor4t.wathe.client.gui.screen.ingame.LimitedInventoryScreen;
+import dev.doctor4t.wathe.cca.PlayerShopComponent;
 import dev.doctor4t.wathe.util.ShopEntry;
-import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.BsXinQin.kinswathe.KinsWatheRoles;
 import org.BsXinQin.kinswathe.KinsWatheShops;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
-@Mixin(LimitedInventoryScreen.class)
-public class BanditShopMixin {
+@Mixin(PlayerShopComponent.class)
+public abstract class BanditShopMixin {
 
-    @Shadow @Final public ClientPlayerEntity player;
+    @Shadow public int balance;
+    @Shadow public abstract void sync();
+    @Shadow @Final @NotNull private PlayerEntity player;
 
-    @ModifyVariable(method = "init", at = @At(value = "STORE"), name = "entries")
-    private List<ShopEntry> getBanditShop(List<ShopEntry> originalEntries) {
-        GameWorldComponent gameWorld = GameWorldComponent.KEY.get(player.getWorld());
-        if (gameWorld.isRole(player, KinsWatheRoles.BANDIT)) {
-            return KinsWatheShops.getBanditShop(player.getWorld());
+    @Inject(method = "tryBuy", at = @At("HEAD"), cancellable = true)
+    void tryBuy(int index, @NotNull CallbackInfo ci) {
+        GameWorldComponent gameWorld = GameWorldComponent.KEY.get(this.player.getWorld());
+        if (gameWorld.isRole(this.player, KinsWatheRoles.BANDIT)) {
+            List<ShopEntry> shop = KinsWatheShops.getBanditShop(this.player.getWorld());
+            if (index < 0 || index >= shop.size()) return;
+            ShopEntry entry = shop.get(index);
+            if (KinsWatheShops.handlePurchase(this.player, this.balance, entry.stack().getItem(), entry.price())) {
+                this.balance -= entry.price();
+                this.sync();
+                ci.cancel();
+            }
         }
-        return originalEntries;
     }
 }
